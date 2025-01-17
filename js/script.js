691e7bedab19c02814af565afef59409adda24da
Javoxirbek Xolmirzayev <javoxirxolmirzayev5@gmail.com>
2025-01-18 01:42:35 +0500

    add project files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let usdToSum = 13000;
+let euroToSum = 14000;
+let rublToSum = 600;
+
+let form = document.querySelector("#form");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  let currencyInpValue = +document.querySelector("#currencyInp").value;
+  let currencyOutputInp = document.querySelector("#currencyOutput");
+  let select = e.target[2].value;
+
+  if (select === "usd") {
+    currencyOutputInp.value = currencyInpValue * usdToSum;
+  } else if (select === "euro") {
+    currencyOutputInp.value = currencyInpValue * euroToSum;
+  } else if (select === "rubl") {
+    currencyOutputInp.value = currencyInpValue * rublToSum;
+  }
+}
